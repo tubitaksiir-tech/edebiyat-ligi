053b83294539338556373004a30f948abbb22bef
tubitaksiir-tech <tubitaksiir@gmail.com>
2025-12-20 03:19:14 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -185,7 +185,7 @@ card_bg_color = "#2e5a27"
 text_color_cream = "#fffbe6"
 red_warning_color = "#c62828"
 input_bg_color = "#3e7a39"
-bg_image_url = "https://i.hizliresim.com/3wghq44.png"
+bg_image_url = "https://i.hizliresim.com/dyq7syw.png"
 
 st.markdown(f"""
     <style>
